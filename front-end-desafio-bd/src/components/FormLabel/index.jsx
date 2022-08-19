307138859fef389694e4772c39c0b2232bc0f5a2
rogenis <rogenis.silva@meliuz.com.br>
2022-08-19 15:32:24 -0400

    Remove old project

--- front-end-desafio-bd/src/components/FormLabel/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function FormLabel ({ title, name, value, onChange, error, errormessage, onBlur }) {
-  return (
-    <div className='form-group mb-3'>
-        <label>{title}</label>
-        <input type="text" name={name} onChange={onChange} value={value} className="form-control"  errormessage={errormessage} onBlur={onBlur}/>
-        <span className="text-danger">{error}</span>
-    </div>
-  );
-}
